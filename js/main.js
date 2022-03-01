87de98712d0ca6457c846d8a9fd44f3acb0dd4a8
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-03-01 03:00:37 +0300

    added Percentage performance

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 $(document).ready(()=>{
   let score=0
+  let questions = 0
 
   fetch("questions.json").then(results=>results.json()).then(data=>loadQuestions(data))
     
@@ -18,8 +19,28 @@ $(document).ready(()=>{
         
         })
 
-       $("#page").html('<h1 class="text-center">Your Score is '+score+'</h1>')  
+       $("#page").html('<h1 class="text-center">Your Score is '+score+' correct out of '+questions+' questions </h1>')
+       $("#page").append(`<h1 class="text-center">${(score/questions)*100}%</h1>`)
+       $("#page").append(`<h1 class="text-center">${(score/questions)*100}%</h1>`)   
     })
+
+
+    const loadQuestions= data=>{
+        $("ol").html(
+           data.map((data,index)=>`<li>${data.question} ${loadAnswerOptions(data.answers,index)} </li>`).join("")
+        ) 
+        questions= $("li").length                  
+    }
+
+    const loadAnswerOptions = (answers,index)=>{
+                            return answers.map((item,id)=>`<div class="form-check">
+                            <input class="form-check-input" type="radio" name="question${index}"  value="${item.correct==true?"correct":"wrong"}" id="question1${id}">
+                                <label class="form-check-label" for="question1${id}">
+                                ${item.answer}
+                                    </label>
+                            </div>`).join("")
+        
+    } 
                
 })
 
@@ -27,19 +48,5 @@ $(document).ready(()=>{
 
 
 
-const loadQuestions= data=>{
-                    $("ol").html(
-                       data.map((data,index)=>`<li>${data.question} ${loadAnswerOptions(data.answers,index)} </li>`).join("")
-                    )                   
-                }
-
-const loadAnswerOptions = (answers,index)=>{
-     return answers.map((item,id)=>`<div class="form-check">
-                                <input class="form-check-input" type="radio" name="question${index}"  value="${item.correct==true?"correct":"wrong"}" id="question1${id}">
-                                    <label class="form-check-label" for="question1${id}">
-                                    ${item.answer}
-                                        </label>
-                                </div>`).join("")
-                  
-                            }                    
+                   
 
